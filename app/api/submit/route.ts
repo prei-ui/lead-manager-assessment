b216ffed059562adaac3a006371894e4c8338f0c
Mackenzie Kustiro <workflow@principlesrei.com>
2026-03-05 17:20:16 -0500

    Fix private key parsing

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -41,7 +41,7 @@ export async function POST(req: NextRequest) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        private_key: (process.env.GOOGLE_PRIVATE_KEY || '').split('\\n').join('\n'),
       },
       scopes: ['https://www.googleapis.com/auth/drive'],
     });
